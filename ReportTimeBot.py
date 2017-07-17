321ebf04d27da95a06030e9253cd3e295596c9a9
truth <truth@changingtec.com>
2017-07-17 15:09:21 +0800

    new version test ok

--- ReportTimeBot.py
+++ ReportTimeBot.py
@@ -5,10 +5,9 @@ import json
 import os.path
 sys.path.append('..\\PTTCrawlerLibrary')
 import PTT
-print('Welcome to 準點報時機器人 v 1.0.17.0714')
+print('Welcome to 準點報時機器人 v 1.0.17.0717')
 
 Board = "Test"
-Retry = True
 
 # If you want to automatically login define Account.txt
 # {"ID":"YourID", "Password":"YourPW"}
@@ -22,15 +21,29 @@ except FileNotFoundError:
     ID = input('Input ID: ')
     Password = getpass.getpass('Input password: ')
 
-def readTimeFile(Time):
-    with open(Time.replace(':', '') + '.txt', encoding = 'utf-8-sig') as TimeFile:
-        PostContentList = TimeFile.readlines()
-    
-    PostContentList = [x.strip() for x in PostContentList]
-    result = '\r\n'.join(PostContentList)
-    return result
-    
 PTTCrawler = PTT.Crawler(ID, Password, False)
+
+def readNextMinFile(Time):
+    
+    MinTemp = int(Time[3:]) + 1
+    HourTemp = int(Time[:2])
+    
+    if MinTemp >= 60:
+        MinTemp = MinTemp % 60
+        HourTemp = (HourTemp + 1) % 24
+    
+    StringHour = str(HourTemp)
+    if len(StringHour) == 1:
+        StringHour = '0' + StringHour
+    
+    StringMin = str(MinTemp)
+    if len(StringMin) == 1:
+        StringMin = '0' + StringMin
+    
+    NextTime = StringHour + StringMin
+    
+    return PTTCrawler.readPostFile(NextTime + '.txt') , StringHour + ':' + StringMin
+
 if not PTTCrawler.isLoginSuccess():
     PTTCrawler.Log('Login fail')
 else:
@@ -39,71 +52,71 @@ else:
     First = True
     LastTime = ''
     
-    WaitNexMin = False
-    StartTime = 0
+    while True:
     
-    while Retry:
         try:
+        
+            ErrorCode, Time = PTTCrawler.getTime()
+            if ErrorCode != PTTCrawler.Success:
+                PTTCrawler.Log('取得時間失敗')
+                sys.exit()
             
-            while True:
+            FileData, NextTimeString = readNextMinFile(Time)
+            if FileData != None:
+                FileDataList = FileData.split('\r\n')
                 
-                if WaitNexMin:
-                    WaitNexMin = False
-                    EndTime = time.time()
-                    
-                    while EndTime - StartTime < 45:
-                        time.sleep(45 - (EndTime - StartTime))
-                        EndTime = time.time()
-                    
+                Title = FileDataList[0]
+                
+                Content = '\r\n'.join(FileDataList[1:])
+                
+                Title = Title.replace('{TIME}', NextTimeString)
+                Content = Content.replace('{TIME}', NextTimeString)
+                
+                PTTCrawler.Log('偵測到報時檔案，將在一分鐘後在 ' + Board + ' 發文')
+                
+            if First:
+                PTTCrawler.Log('成功啟動! 與 PTT 同步時間中')
+                LastTime = Time
+            else:
+                time.sleep(50)
+            
+            while LastTime == Time:
                 ErrorCode, Time = PTTCrawler.getTime()
                 if ErrorCode != PTTCrawler.Success:
-                    PTTCrawler.Log('Get time error')
-                    continue
-                if len(Time) < 5:
-                    Time = '0' + Time
-                if LastTime != Time:
-                    LastTime = Time
-                    if First:
-                        First = False
-                        PTTCrawler.Log('Start detect PTT time')
-                        continue
-                    break
-            StartTime = time.time()
-            WaitNexMin = True
+                    PTTCrawler.Log('取得時間失敗')
+                    sys.exit()
+            LastTime = Time
+            if First:
+                First = False
+                PTTCrawler.Log('成功同步')
             
-            PTTCrawler.Log('Ptt time: ' + Time + '!')
-
-            PostContent = PTTCrawler.readPostFile(Time.replace(':', '') + '.txt')
-            if PostContent == None:
+            PTTCrawler.Log(Time)
+            
+            if FileData == None:
                 continue
             
-            ErrorCode = PTTCrawler.post(Board, '準點報時 ' + Time, PostContent, 2, 0)
+            ErrorCode = PTTCrawler.post(Board, Title, Content, 2, 0)
             if ErrorCode == PTTCrawler.Success:
-                PTTCrawler.Log('Post in ' + Board + ' success')
-                PTTCrawler.Log(PostContent)
+                PTTCrawler.Log('在 ' + Board + ' 板發文成功')
             elif ErrorCode == PTTCrawler.NoPermission:
                 PTTCrawler.Log('發文權限不足')
             else:
-                PTTCrawler.Log('Post in Test fail')
-            
+                PTTCrawler.Log('在  ' + Board + ' 板發文失敗') 
         except KeyboardInterrupt:
-            '''
-            exc_info = sys.exc_info()
-            traceback.print_exception(*exc_info)
-            '''
-            PTTCrawler.Log('Interrupted by user')
+            
+            PTTCrawler.Log('使用者中斷')
             PTTCrawler.logout()
             sys.exit()
         except EOFError:
             exc_info = sys.exc_info()
             traceback.print_exception(*exc_info)
-            Retry = True
+            Running = True
             break
         except ConnectionAbortedError:
-            Retry = True
+            Running = True
             break
         except Exception:
             exc_info = sys.exc_info()
             traceback.print_exception(*exc_info)
-            Retry = True
+            Running = True
             break
